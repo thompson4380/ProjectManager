e2accba0c03a7d07efab975c8a8a9958bb019e6e
Thomas Ostermann <t.ostermann@txonline.de>
2021-01-03 17:19:54 +0100

    Added ProjectListView

---
+++ ProjectManager/ProjectListView.swift
@@ -0,0 +1,20 @@
+//
+//  ProjectListView.swift
+//  ProjectManager
+//
+//  Created by Thomas Ostermann on 03.01.21.
+//
+
+import SwiftUI
+
+struct ProjectListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ProjectListView_Previews: PreviewProvider {
+    static var previews: some View {
+        ProjectListView()
+    }
+}
